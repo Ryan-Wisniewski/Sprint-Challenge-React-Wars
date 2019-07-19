82be87a007abce4171c038640b482505f996a897
Ryan-Wisniewski <Ryanwis98@yahoo.com>
2019-07-19 16:18:05 -0400

    dont think im supposed to use res to map.. about to make a useState index again. The only part im stuck on is IDK what to do with the [0] in setNames(res[0].name)

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -1,6 +1,5 @@
 import React, {useState, useEffect} from 'react';
 import axios from 'axios'
-import {StarWarsPeople} from './components/StarWarsPeople'
 
 import './App.css';
 
@@ -10,43 +9,36 @@ import './App.css';
   // Fetch characters from the star wars api in an effect hook. Remember, anytime you have a 
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
-let i = 2
-  // const
+
 const App = () => {
- const [names, setNames] = useState([])
- const [nums, setNums] = useState(names)
-//  const [index, setIndex] = useState([names])
-console.log('checkName State', names)
+  const [names, setNames] = useState([])
+
   useEffect(() => {
     axios.get('https://swapi.co/api/people/')
     .then(response => {
       const res = response.data.results
-      for (let i = 0; i < names.length; i++){
-        console.log('iHere: ', i)
-        return names[i]       
-      }
-      setNames(res[i].name)
-      // console.log('dataObj', res) 
-      // console.log('nameresult', res  )
-      console.log('names = ', names)
+      console.log('dataObj', res ) 
+      // res.map(names => {return <div>{names}</div>})
+      setNames(res[0].name)
+      {res.map((names) => {return {names}})}
+      // console.log('checkHere', index)
+      
     })
      .catch(error => {
        console.log("Server API not functioning", error)
-     })     
-  },[nums])
-console.log('reeeee', names)
-// console.log('whyyyy', nums)
-// console.log('reeeeendex', index)
+     }) 
 
-  return (
     
+  },[names])
+console.log('OutsideNames', names)
+  return (
     <div className="App">
       <h1 className="Header">React Wars</h1>
-        {/* {names} */}
-        {nums.map((names => {return <StarWarsPeople info={names} />}))}
-        {/* {index} */}
-        <StarWarsPeople info={names}/>
-
+      <div>
+      {names}
+      
+      {/* {nums.map((names => {return <StarWarsPeople info={names} />}))} */}
+      </div>
     </div>
   );
 }
