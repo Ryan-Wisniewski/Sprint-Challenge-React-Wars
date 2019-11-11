53824cfedddbf2bf00f7c0b307e641cd79d1a2e2
Ryan-Wisniewski <Ryanwis98@yahoo.com>
2019-11-11 18:26:38 -0500

    change

---
+++ starwars/src/components/StarWarsPeople.js
@@ -0,0 +1,34 @@
+import React from 'react'
+import { PeopleStyle } from './Styles';
+
+// export function StarWarsPeople(props){
+//     console.log(props)
+//     return(
+//         props.info.map(person => {
+//             return props.info={person}
+//         })
+//         // {nums.map((names => {return <StarWarsPeople info={names} />}))}
+//     )}
+
+    // export const StarWarsPeople = (props) => { 
+    //     // console.log('props', props)
+    //     return(
+    //     <div>
+    //     <p>{props.info}</p>
+    //     <p>Hardcode removeme</p>
+    //     </div> 
+var sdf = 'sdf'
+var num = 1+1
+
+    export function StarWarsPeople(props){
+        console.log('props', props)
+        console.log('data', props.info)
+        return(
+            <div className='container'>
+                {props.info.map(data =>
+               { return <PeopleStyle>{data.name}</PeopleStyle>}       
+            )}
+            </div>
+        )}
+
+let x =x
